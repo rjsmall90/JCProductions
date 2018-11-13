d508973ef11b054bebde4596640f8a2803c39ca1
Admin <admin@Admins-Macintosh-3.local>
2018-11-13 10:53:38 -0500

    home page setup

---
+++ jcpdx/src/components/bio/bio.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export class Bio extends Component {
+    render() {
+      return (
+        <React.Fragment>
+
+
+        </React.Fragment>
+        );
+    }
+}
+  
+
+  export default Bio
